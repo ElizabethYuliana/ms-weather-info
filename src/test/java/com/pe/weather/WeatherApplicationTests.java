823afbd6cf78c20d1d1ad160a0a533240d42ed8f
Elizabeth <elizabeth.yuliana@hotmail.com>
2025-06-14 16:43:05 -0500

    FEAT: Update test configuration to exclude Mongo auto-configuration

--- src/test/java/com/pe/weather/WeatherApplicationTests.java
+++ src/test/java/com/pe/weather/WeatherApplicationTests.java
@@ -3,8 +3,9 @@ package com.pe.weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.cloud.azure.keyvault.secret.property-source-enabled=false")
-class WeatherApplicationTests {
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration,org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration"
+})class WeatherApplicationTests {
 
 	@Test
 	void contextLoads() {
