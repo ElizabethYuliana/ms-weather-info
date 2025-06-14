2f0637a03402f6e8b829644753543d6d3558874c
Elizabeth <elizabeth.yuliana@hotmail.com>
2025-06-14 16:22:50 -0500

    FEAT: Update application properties and add Azure Key Vault configuration

--- src/test/java/com/pe/weather/WeatherApplicationTests.java
+++ src/test/java/com/pe/weather/WeatherApplicationTests.java
@@ -3,7 +3,7 @@ package com.pe.weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.cloud.azure.keyvault.secret.property-source-enabled=false")
 class WeatherApplicationTests {
 
 	@Test
